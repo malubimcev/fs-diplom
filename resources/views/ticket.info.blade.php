ca0d591953bf7d84dfde3487f490a76f47d95d28
malubimcev <malubimcev@gmail.com>
2019-06-23 19:10:47 +0000

    добавлен csrf-token в meta-тег

--- resources/views/ticket.info.blade.php
+++ resources/views/ticket.info.blade.php
@@ -2,15 +2,23 @@
 
 @section('ticket-info')
     <p class="ticket__info">На фильм: 
-        <span class="ticket__details ticket__title">{{ $ticket->movie()->title }}</span>
+        <span class="ticket__details ticket__title">
+            {{ $ticket->movie()->title }}
+        </span>
     </p>
     <p class="ticket__info">Места: 
-        <span class="ticket__details ticket__chairs">{{ $ticket->seats() }}</span>
+        <span class="ticket__details ticket__chairs">
+            {{ $ticket->seats() }}
+        </span>
     </p>
     <p class="ticket__info">В зале: 
-        <span class="ticket__details ticket__hall">{{ $ticket->hall()->title }}</span>
+        <span class="ticket__details ticket__hall">
+            {{ $ticket->hall()->title }}
+        </span>
     </p>
     <p class="ticket__info">Начало сеанса: 
-        <span class="ticket__details ticket__start">{{ $ticket->seance()->start_time }}</span>
+        <span class="ticket__details ticket__start">
+            {{ $ticket->seance()->start_time }}
+        </span>
     </p>
 @endsection
