952e047aed61e6aeb0df2a726462cea8ddbfc9d9
malubimcev <malubimcev@gmail.com>
2019-07-02 18:43:03 +0000

    добавлено поле is_open

--- database/migrations/2019_05_28_071449_create_halls_table.php
+++ database/migrations/2019_05_28_071449_create_halls_table.php
@@ -20,6 +20,7 @@ class CreateHallsTable extends Migration
             $table->smallInteger('seats_count')->default(0)->unsigned();
             $table->double('price')->default(0);
             $table->double('price_vip')->default(0);
+            $table->boolean('is_open')->default(false);
             $table->timestamps();
         });
     }
